6d1f565dc0326940e6a89fd0ee2605b42204eaa3
Bhawna Saroha <30bhawna95@gmail.com>
2018-09-18 00:45:13 +0530

    express rotue

--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,15 @@ let app = express();
 const data = require('../data/server');
 
 // respond with file data when a GET request is made 
-app.get('/getPosts', function (req, res) {
-  res.send();
+// app.get('/getPosts', function (req, res) {
+//   res.render('http://127.0.0.1:4000/');
+// });
+
+var __dirname = '/';
+var db = {};
+app.get('/data/db.json', (req, res) => {
+  console.log(res);
+
+  /* Just send the file */
+  res.sendFile(path.join(__dirname, '/src/data', 'db.json'));
 });
